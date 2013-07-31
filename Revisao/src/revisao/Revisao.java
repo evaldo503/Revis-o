dbad3792669cb9588499777d1b0d2009cce881dc
bruna_zakrzeski <bruna_zakrzeski@10.1.49.213>
2013-07-31 14:37:18 -0300

    Fazendo execução de variaveis como INT E STRING .

--- Revisao/src/revisao/Revisao.java
+++ Revisao/src/revisao/Revisao.java
@@ -1,6 +1,8 @@
 
 package revisao;
 
+import javax.swing.JOptionPane;
+
 public class Revisao {
 
     public static void main(String[] args) {
@@ -8,6 +10,17 @@ public class Revisao {
      String nome;
      int idade;
      String dataNascimento;
+     
+     
+     nome = JOptionPane.showInputDialog("Digite seu nome: ");
+     idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade: "));
+     dataNascimento = JOptionPane.showInputDialog("Digite sua data de nascimento: ");
+     
+     
+     
+     System.out.println("Nome do Paciente: " + nome + "\nIdade do Paciente: " + idade
+             + "\nData de Nascimento do Paciente: " + dataNascimento);
+     
      
     }
 }
