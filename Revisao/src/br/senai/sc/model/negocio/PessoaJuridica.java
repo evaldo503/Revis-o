45618eb471aaf72b2f241f7d49547b2846426cd7
bruna_zakrzeski <bruna_zakrzeski@10.1.49.213>
2013-07-31 16:02:29 -0300

    Adicionando outra classe com o nome de PessoaJuridica .

---
+++ Revisao/src/br/senai/sc/model/negocio/PessoaJuridica.java
@@ -0,0 +1,24 @@
+
+package br.senai.sc.model.negocio;
+
+
+/** 
+ * Classe representa as pessoas jurídica do sistema
+ * @version 1.0   31/07/2013
+ * @author bruna_zakrzeski
+ */
+
+public class PessoaJuridica {
+    
+    
+    private String cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+
+    public PessoaJuridica() {
+    }
+    
+    
+
+    
+}
