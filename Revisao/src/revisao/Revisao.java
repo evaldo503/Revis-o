d85083c6a2e73770a76fd58e425ad83cd7edc52b
bruna_zakrzeski <bruna_zakrzeski@10.1.49.213>
2013-07-31 14:25:20 -0300

    Elaborando variavéis .

--- Revisao/src/revisao/Revisao.java
+++ Revisao/src/revisao/Revisao.java
@@ -1,19 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package revisao;
 
-/**
- *
- * @author bruna_zakrzeski
- */
 public class Revisao {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+     String nome;
+     int idade;
+     String dataNascimento;
+     
     }
 }
