f7a5623d599d840717a3b7c202eae9bc0716213c
bruna_zakrzeski <bruna_zakrzeski@10.1.49.213>
2013-07-31 14:50:40 -0300

    Fazendo dados de uma pessoa .

---
+++ Revisao/src/br/senai/sc/model/negocio/Pessoa.java
@@ -0,0 +1,20 @@
+
+package br.senai.sc.model.negocio;
+
+/** 
+ * Essa classe representa as pessoas do software
+ * @version 1.0   31/07/2013
+ * @author bruna.zakrzeski
+ */
+
+
+public class Pessoa {
+    
+    private int id;
+    private String endereco;
+    private String telefone;
+    private String dataCadastro;
+    
+    
+    
+}
