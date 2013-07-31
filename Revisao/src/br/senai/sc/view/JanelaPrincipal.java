d6283896b3c0f96012dc9f4d47a65cf780e0b5c6
bruna_zakrzeski <bruna_zakrzeski@10.1.49.213>
2013-07-31 17:18:35 -0300

    Adicionei menu na Janela Principal.

---
+++ Revisao/src/br/senai/sc/view/JanelaPrincipal.java
@@ -0,0 +1,41 @@
+package br.senai.sc.view;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Classe que controla toda a parte visual da aplicação
+ *
+ * @version 1.0 31/07/2013
+ * @author bruna_zakrzeski
+ */
+public class JanelaPrincipal {
+
+    public void mostraMenu() {
+        
+       int opcao;
+        
+
+        do {
+            opcao - Integer.parseInt
+                    JOptionPane.showInputDialog("Informe a opção:"
+                            + "\n1 - Funcionário"
+                            + "\n2 - Cliente"
+                            + "\n3 - Fornecedor
+                            + "\n4 - Transportadora"
+       
+        
+        switch (opcao) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inexistente! ");
+        }
+    }
+}
